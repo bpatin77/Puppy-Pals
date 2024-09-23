e197e22a40717d8da7ab3d5bcdcf97e92d89751a
Blake <bpatin77@gmail.com>
2024-09-23 17:25:29 -0500

    Puppy Pals done

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,39 @@ import "./App.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
   console.log("puppyList: ", puppyList);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
 
   return (
     <div className="App">
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              {
+                setFeatPupId(puppy.id);
+              }
+              console.log("puppy id: ", puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
+export default App;
